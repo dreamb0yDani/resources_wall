cfb0fb9357798b709cf7f0e93e0f4b0680a74abb
Dawood <dreamboy.dani@gmail.com>
2020-09-20 23:07:29 +0000

    Add the seeds

--- db/seeds/03_resources.sql
+++ db/seeds/03_resources.sql
@@ -1,10 +1,40 @@
-INSERT INTO resources (url, title, description, user_id, category_id) VALUES ('https://www.codecademy.com/', 'Learn to code here!', 'I thought this site was really helpful and just wanted to share!!!', 1, 1);
-INSERT INTO resources (url, title, description, user_id, category_id) VALUES ('https://screenrant.com/dune-2020-movie-trailer-criticism-alejandro-jodorowsky/', 'Not everyone is a fan!', 'I thought it looked neat, but check out what this former Dune director thinks', 1, 2);
-INSERT INTO resources (url, title, description, user_id, category_id) VALUES ('https://www.w3schools.com/sql/sql_join.asp', 'Click here if you love Venn diagrams...', 'I used to love them.', 2, 1);
-INSERT INTO resources (url, title, description, user_id, category_id) VALUES ('https://www.cinemablend.com/news/2555099/even-john-david-washington-reveals-he-cant-totally-explain-tenet', "At least I'm not alone!", 'Apparently this movie is incomprehensible even to the cast', 2, 2);
-INSERT INTO resources (url, title, description, user_id, category_id) VALUES ('https://launchschool.com/books/command_line/read/introduction#briefoverview', 'Wooooooooow!', 'Who knew the command line was so complicated?!?', 3, 1);
-INSERT INTO resources (url, title, description, user_id, category_id) VALUES ('https://nypost.com/2020/09/20/halloween-2020-will-reportedly-be-brightened-by-a-rare-blue-moon/', 'Blue moon', 'This is so cool!', 3, 2);
-INSERT INTO resources (url, title, description, user_id, category_id) VALUES ('http://jlord.us/git-it/index.html', 'Git it? Got it! Good!', 'For anyone looking to become a git-head', 4, 1);
-INSERT INTO resources (url, title, description, user_id, category_id) VALUES ('https://screenrant.com/wheel-time-tv-show-video-two-rivers-set/', 'PLEASE let this be good', 'I am such a WoT fan and really, really, REALLY want to be able to enjoy this show', 4, 2);
-INSERT INTO resources (url, title, description, user_id, category_id) VALUES ('https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration', "I'm feeling a bit loopy", 'This should help you get started on learning to loop', 5, 1);
-INSERT INTO resources (url, title, description, user_id, category_id) VALUES ('https://www.usatoday.com/story/money/2020/09/03/walmart-toy-list-2020-kids-interactive-play-christmas-holiday-gifts/5694280002/', 'Christmas is just around the corner!', 'Gift ideas for your kids! Or for you if you love Baby Yoda', 5, 2);
+INSERT INTO resources
+  (url, title, description, user_id, category_id)
+VALUES
+  ('https://www.codecademy.com/', 'Learn to code here!', 'I thought this site was really helpful and just wanted to share!!!', 1, 1);
+INSERT INTO resources
+  (url, title, description, user_id, category_id)
+VALUES
+  ('https://screenrant.com/dune-2020-movie-trailer-criticism-alejandro-jodorowsky/', 'Not everyone is a fan!', 'I thought it looked neat, but check out what this former Dune director thinks', 1, 2);
+INSERT INTO resources
+  (url, title, description, user_id, category_id)
+VALUES
+  ('https://www.w3schools.com/sql/sql_join.asp', 'Click here if you love Venn diagrams...', 'I used to love them.', 2, 1);
+INSERT INTO resources
+  (url, title, description, user_id, category_id)
+VALUES
+  ('https://www.cinemablend.com/news/2555099/even-john-david-washington-reveals-he-cant-totally-explain-tenet', 'At least Im not alone', 'Apparently this movie is incomprehensible even to the cast', 2, 2);
+INSERT INTO resources
+  (url, title, description, user_id, category_id)
+VALUES
+  ('https://launchschool.com/books/command_line/read/introduction#briefoverview', 'Wooooooooow!', 'Who knew the command line was so complicated?!?', 3, 1);
+INSERT INTO resources
+  (url, title, description, user_id, category_id)
+VALUES
+  ('https://nypost.com/2020/09/20/halloween-2020-will-reportedly-be-brightened-by-a-rare-blue-moon/', 'Blue moon', 'This is so cool!', 3, 2);
+INSERT INTO resources
+  (url, title, description, user_id, category_id)
+VALUES
+  ('http://jlord.us/git-it/index.html', 'Git it? Got it! Good!', 'For anyone looking to become a git-head', 4, 1);
+INSERT INTO resources
+  (url, title, description, user_id, category_id)
+VALUES
+  ('https://screenrant.com/wheel-time-tv-show-video-two-rivers-set/', 'PLEASE let this be good', 'I am such a WoT fan and really, really, REALLY want to be able to enjoy this show', 4, 2);
+INSERT INTO resources
+  (url, title, description, user_id, category_id)
+VALUES
+  ('https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration', 'Im feeling a bit loopy', 'This should help you get started on learning to loop', 5, 1);
+INSERT INTO resources
+  (url, title, description, user_id, category_id)
+VALUES
+  ('https://www.usatoday.com/story/money/2020/09/03/walmart-toy-list-2020-kids-interactive-play-christmas-holiday-gifts/5694280002/', 'Christmas is just around the corner!', 'Gift ideas for your kids! Or for you if you love Baby Yoda', 5, 2);
