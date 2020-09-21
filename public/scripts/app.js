29d9a57fc3db6a997c8753a117029e16fb147fb7
Dawood <dreamboy.dani@gmail.com>
2020-09-21 16:56:27 +0000

    Add the ajax for the GET

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,44 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for (user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
-});
+const renderContent = (contents) => {
+  console.log(contents.resources)
+  for (const element of contents.resources) {
+    // console.log(conteelement)
+    $("#content-container").prepend(createContentElement(element))
+  }
+}
+
+const createContentElement = content => {
+  let $content = `
+  <article class="content">
+        <header>
+          <h2>${content.title}</h2>
+          <embed src=${content.url} type="text/html">
+          <p>${content.description}</p>
+        </header>
+      </article>`
+
+  return $content
+}
+
+$(document).ready(() => {
+  console.log('Ready')
+  // $(() => {
+  // $.ajax({
+  //   method: "GET",
+  //   url: "/r",
+  //   dataType: "JSON"
+  // }).done((users) => {
+  //   for (user of users) {
+  //     $("<div>").text(user.name).appendTo($("body"));
+  //   }
+  // });;
+  // });
+  const loadContents = () => {
+    $.ajax("/r", { method: "GET", dataType: "JSON" })
+      .then(dataReceived => {
+        // console.log(dataReceived[0])
+        renderContent(dataReceived)
+      })
+  }
+
+  loadContents();
+})
