34e7f36b10f1186f8cd1b06e26ea2e24dd034030
doge33 <sandylx33@gmail.com>
2020-09-21 05:58:17 +0000

    added resource-related routes & rough templates; still modifying ajax

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,4 @@
+/*
 $(() => {
   $.ajax({
     method: "GET",
@@ -8,3 +9,47 @@ $(() => {
     }
   });;
 });
+*/
+//this helper loops through an array of reviews from database
+const renderReviews = (reviewsArray) => {
+
+  for (let review of reviewsArray) {
+
+    $('#resource-reviews').append(createReviewElement(review));
+  }
+};
+$(document).ready(() => {
+
+  const loadReviews = function() {
+
+    $.ajax({
+      url: '/resources',
+      method:"GET",
+      success: (response) => {
+        $('form').trigger('reset');
+        $('#resource-reviews').empty();
+        renderTweets(response);
+      }
+    });
+  };
+  //GET right after definition to load initial tweets;
+  loadReviews();
+
+  $('#submit-review').on('submit', function(evt) {
+    //form-validation here:
+    //const userInput = $('textArea').val();
+    //$('.error').slideUp();
+      evt.preventDefault();
+      //serialize value of form because server is set to receive query string
+      const  userReview = $(this).serialize();
+
+      $.ajax({
+        url: '/resources/:id/reviews',
+        method: 'POST',
+        dataType: 'text',
+        data: userReview,
+        success: loadReviews
+      });
+    })
+  });
+
--- routes/resources.js
+++ routes/resources.js
@@ -9,6 +9,7 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = (db) => {
+  /*
   router.get("/", (req, res) => {
     let query = `SELECT * FROM resources`;
     console.log(query);
@@ -23,32 +24,142 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+  */
 
 
-  // register route for the user
+  // go to main page
   router.get("/resources", (req, res) => {
     // all the resrouces form the database regardless of the user.
-    res.render("main_page")
+
+    //insert the result of the resources query into template
+    let query = `SELECT * FROM resources`;
+    //console.log(query);
+
+    db.query(query)
+      .then(data => {
+        const resources = data.rows;
+        console.log(resources);
+        const templateVars = {
+          resourceList: resources
+        }
+        res.render("main_page", templateVars);
+
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+
   });
 
 
   router.post("/resources", (req, res) => {
-    // postig the resource to the database.
-    res.redirect("main_page")
+
+    const currentUser = req.session.user_id;
+    const resourceTitle = req.body.title;
+    const resourceUrl = req.body.url;
+    const resourceDescription = req.body.description;
+    const resourceCategory = req.body.category;
+
+    //modularize into addResource function later
+    let query = `
+      INSERT INTO resources (title, url, description, user_id, category_id)
+      VALUES(${resourceTitle}, ${resourceUrl}, ${resourceDescription}, ${currentUser}, ${resourceCategory})
+      RETURNING *
+    `;
+
+    db.query(query)
+      .then(res => {
+        //console.log(res.rows[0]);
+        res.redirect("main_page")   //should we redirect to /resources/:id instead?
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
   });
 
+
   router.get("/users/:id/resources", (req, res) => {
-    // user's resources
-    res.render("user_resrouces")
+    // myResource page!
+
+    //const currentUserID = req.session.user_id;
+    let query = `SELECT resources.* FROM resources JOIN users ON resources.user_id = users.id WHERE users.id = 2`;
+    //console.log(query);
+    db.query(query)
+    .then(data => {
+      const resources = data.rows;
+      //console.log(resources);
+      const templateVars = {
+        resourceList: resources
+      }
+      res.render("user_resources", templateVars);
+
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
   });
+
+
 
   router.get("/resources/:id", (req, res) => {
     // accessa speicific resources
+    const resourcesId = req.params.id;
+
+    let query = `SELECT * FROM resources WHERE resources.id = ${resourcesId}`;
+    console.log(query);
+
+    db.query(query)
+    .then(data => {
+      const specificResource = data.rows[0];
+
+      const templateVars = {
+        aResource: specificResource
+      }
+      res.render('resource', templateVars);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
   });
 
   router.post("/resources/:id/reviews", (res, req) => {
     // posting the reviews for a specific resource
     // form with textarea, button and rating.
+
+
+    const currentUser = req.session.user_id;
+    const reviewComment = req.body.comment;
+    const reviewLiked = req.body.liked; //=> event handler later
+    const reviewRating = req.body.rating;
+    const resourcesId = req.params.id;
+
+    //modularize into addResource function later (->tweeter 'saveTweet' )
+    let query = `
+      INSERT INTO resources (title, url, description, user_id, category_id)
+      VALUES(${reviewComment}, ${reviewLiked}, ${reviewRating}, ${currentUser}, ${resourcesId})
+      RETURNING *
+    `;
+
+    db.query(query)
+      .then(data => {
+        const review = data.rows[0]
+        //console.log(res.rows[0]);
+        res.status(201).send(review); //?
+        })
+        //review to be appended to resource page -->to be appended by event handler
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
 
   });
 
