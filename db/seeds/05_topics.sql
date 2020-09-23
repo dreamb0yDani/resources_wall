6620c2ae8cd2c15d127452cbd65bc5d4b2405b3f
Michael Klepac <mklepac90@gmail.com>
2020-09-23 17:05:42 +0000

    added new topics table and seed

---
+++ db/seeds/05_topics.sql
@@ -0,0 +1,29 @@
+INSERT INTO topics (tag, resource_id) VALUES ('codecademy', 1);
+INSERT INTO topics (tag, resource_id) VALUES ('coding', 1);
+
+INSERT INTO topics (tag, resource_id) VALUES ('python', 2);
+INSERT INTO topics (tag, resource_id) VALUES ('udemy', 2);
+
+INSERT INTO topics (tag, resource_id) VALUES ('joins', 3);
+INSERT INTO topics (tag, resource_id) VALUES ('venn', 3);
+
+INSERT INTO topics (tag, resource_id) VALUES ('js', 4);
+INSERT INTO topics (tag, resource_id) VALUES ('promises', 4);
+
+INSERT INTO topics (tag, resource_id) VALUES ('command line', 5);
+INSERT INTO topics (tag, resource_id) VALUES ('cli', 5);
+
+INSERT INTO topics (tag, resource_id) VALUES ('programming', 6);
+INSERT INTO topics (tag, resource_id) VALUES ('basics', 6);
+
+INSERT INTO topics (tag, resource_id) VALUES ('git', 7);
+INSERT INTO topics (tag, resource_id) VALUES ('git init', 7);
+
+INSERT INTO topics (tag, resource_id) VALUES ('devtools', 8);
+INSERT INTO topics (tag, resource_id) VALUES ('chrome', 8);
+
+INSERT INTO topics (tag, resource_id) VALUES ('looping', 9);
+INSERT INTO topics (tag, resource_id) VALUES ('js', 9);
+
+INSERT INTO topics (tag, resource_id) VALUES ('node', 10);
+INSERT INTO topics (tag, resource_id) VALUES ('free', 10);
